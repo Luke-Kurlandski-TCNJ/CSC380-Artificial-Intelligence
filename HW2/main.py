0545ddf950e7c58392d39d62fc2fec47431fb62a
Luke-Kurlandski-TCNJ <kurlanl1@tcnj.edu>
2021-09-24 12:48:25 -0400

    1.2, 1.3, 1.4

--- HW2/main.py
+++ HW2/main.py
@@ -1,11 +1,17 @@
+from statistics import mean
+
 import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import LinearSVC
 
-def results_df(labels, pred, probas, score):
-	print(f"Mean score: {score}")
+def results_df(labels, pred, probas):
 	df = pd.DataFrame({
 		'label' : labels,
 		'pred' : pred, 
@@ -13,33 +19,79 @@ def results_df(labels, pred, probas, score):
 		'class 2 prob' : probas[1],
 		'class 2 prob' : probas[2],
 	})
-	print(df)
 	return df
 
 def one_point_two():
 	X, y = load_iris(return_X_y=True)
+	X_train, X_test, y_train, y_test = train_test_split(
+		X, y, test_size=0.25, random_state=0)
 
 	clf = LogisticRegression(random_state=0, max_iter=1000)
-	clf = clf.fit(X, y)
+	clf = clf.fit(X_train, y_train)
 
-	pred = clf.predict(X)
-	probas = clf.predict_proba(X).transpose()
-	score = clf.score(X, y)
+	print(f"Score: {clf.score(X_test, y_test)}")
 
-	results_df(y, pred, probas, score)
+	pred = clf.predict(X_test)
+	probas = clf.predict_proba(X_test).transpose()
+	print(results_df(y_test, pred, probas))
 
 def one_point_three():
 	X, y = load_iris(return_X_y=True)
+	X_train, X_test, y_train, y_test = train_test_split(
+		X, y, test_size=0.25, random_state=0)
 
 	clf = GaussianNB()
-	clf.fit(X, y)
+	clf.fit(X_train, y_train)
 
-	pred = clf.predict(X)
-	probas = clf.predict_proba(X).transpose()
-	score = clf.score(X, y)
+	print(f"Score: {clf.score(X_test, y_test)}")
 
-	results_df(y, pred, probas, score)
+	train_sizes, train_scores, valid_scores = \
+		learning_curve(clf, X_train, y_train, cv=5)
+
+	train_scores_mean = np.mean(train_scores, axis=1)
+	valid_scores_mean = np.mean(valid_scores, axis=1)
+
+	plt.close()
+	plt.plot(train_sizes, train_scores_mean, label="Training Set")
+	plt.plot(train_sizes, valid_scores_mean, label="CV Set")
+	plt.legend()
+	plt.grid()
+	plt.title("Learning Curve for Iris Dataset")
+	plt.xlabel("Training Data")
+	plt.ylabel("Accuracy")
+	plt.savefig("one_point_three.png")
+
+	pred = clf.predict(X_test)
+	probas = clf.predict_proba(X_test).transpose()
+
+	print(results_df(y_test, pred, probas))
+
+def one_point_four():
+	X, y = load_iris(return_X_y=True)
+	X_train, X_test, y_train, y_test = train_test_split(
+		X, y, test_size=0.25, random_state=0)
+
+	clf = LinearSVC(max_iter=10000)
+
+	print(mean(cross_val_score(clf, X, y, cv=5)))
+
+	train_sizes, train_scores, valid_scores = \
+		learning_curve(clf, X_train, y_train, cv=5)
+
+	train_scores_mean = np.mean(train_scores, axis=1)
+	valid_scores_mean = np.mean(valid_scores, axis=1)
+
+	plt.close()
+	plt.plot(train_sizes, train_scores_mean, label="Training Set")
+	plt.plot(train_sizes, valid_scores_mean, label="CV Set")
+	plt.legend()
+	plt.grid()
+	plt.title("Learning Curve for Iris Dataset")
+	plt.xlabel("Training Data")
+	plt.ylabel("Accuracy")
+	plt.savefig("one_point_four.png")
 
 if __name__ == "__main__":
 	#one_point_two()
 	one_point_three()
+	one_point_four()
