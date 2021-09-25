411471109a1582e622ee732bad2bd400e77ee8da
msunseri <90657142+msunseri@users.noreply.github.com>
2021-09-24 21:00:24 -0400

    Update main.py

--- HW2/main.py
+++ HW2/main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import load_iris
+from sklearn.datasets import load_digits
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
