5fcc2a0dccd56f34103821f452df2687491c12f1
uthapa <thapau1@tcnj.edu>
2021-09-27 20:13:20 -0400

    2.2

--- HW3/main.py
+++ HW3/main.py
@@ -1,6 +1,12 @@
-# 2.2 PyTorch 
+# 2.2 PyTorch tutorials 
+
 import torch 
 import numpy as np
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+
 
 data =[[1,2],[3,4]]
 x_data = torch.tensor(data)
@@ -28,4 +34,64 @@ print(f"Zeros Tensor: \n {zeros_tensor} \n")
 tensor = torch.rand(3,4)
 print(f"Shape of tensor: {tensor.shape}")
 print(f"Datatype of tensor: {tensor.dtype} ")
-print(f"Device tensor is stored on: {tensor.device} ")
+print(f"Device tensor is stored on: {tensor.device} \n")
+
+# Mathematical operations 
+r = torch.rand(2,2) - 0.5 * 2 #values between -1 and 1
+print('A random matrix, r:')
+print(r)
+
+# Linear algebra opertaions like determinant and singular value decomposition
+print('\nDeterminant of r :')
+print(torch.det(r))
+print('\nSingulr value decomposition of r: ')
+print(torch.svd(r))
+
+#stats
+print('\nAverage and standard deviation of r: ')
+print(torch.std_mean(r))
+print('\nMaximum value of r')
+print(torch.max(r))
+
+#Datasets and DataLoaders
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+#iterating and visualizing the Dataset
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot",
+}
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(len(training_data), size=(1,)).item()
+    img, label = training_data[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
+
+
+
+
